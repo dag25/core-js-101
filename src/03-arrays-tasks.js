0ec0d4dd0f55d998b567cb918d4fc462bab4501a
dag <45577604+dag25@users.noreply.github.com>
2024-10-24 19:34:40 +0400

    Update 03-arrays-tasks.js

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -233,10 +233,8 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(arr) {
- let sum = 0;
-  // eslint-disable-next-line no-return-assign
-  return arr.map((x) => (sum += x));
+function getMovingSum(/* arr */) {
+  throw new Error('Not implemented');
 }
 
 /**
